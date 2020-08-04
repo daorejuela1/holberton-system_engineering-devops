e1ac34979958e4ad24f61e771bfa4d80fe6049ad
daorejuela1 <vids@hotmail.es>
2020-08-04 01:28:34 +0000

    added mandatory tasks for ssh project

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,1 +1,12 @@
-#!/usr/bin/env bash
+# This manifesto will append a line if not exist to the ssh config file
+file_line { 'Turn off passwd auth':
+ensure => 'created',
+path   => '~/.ssh/holberton',
+line   => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+ensure => 'created',
+path   => '~/.ssh/holberton',
+line   => 'IdentityFile ~/.ssh/holberton',
+}
