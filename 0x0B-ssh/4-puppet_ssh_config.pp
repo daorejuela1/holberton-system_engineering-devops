6aed53414812416fe42c2709eb8b08dce429ed1c
daorejuela1 <vids@hotmail.es>
2020-08-04 05:22:24 +0000

    modified advance task typo created to present

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,12 +1,13 @@
 # This manifesto will append a line if not exist to the ssh config file
+
 file_line { 'Turn off passwd auth':
-ensure => 'created',
+ensure => 'present',
 path   => '~/.ssh/holberton',
 line   => 'PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
-ensure => 'created',
+ensure => 'present',
 path   => '~/.ssh/holberton',
 line   => 'IdentityFile ~/.ssh/holberton',
 }
