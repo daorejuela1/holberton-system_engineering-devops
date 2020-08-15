8b8fbeb7430901f79993c752231aee343917e25d
daorejuela1 <vids@hotmail.es>
2020-08-15 02:59:38 +0000

    test with extern file

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,28 +1,17 @@
 #This pupppet scripts assure an nginx installation
-
-exec { '/usr/bin/env apt-get -y update':}
-package {'nginx':
-ensure => present,
-name   => 'nginx',
+exec { '/usr/bin/env apt-get -y update' : }
+-> package { 'nginx':
+  ensure => installed,
 }
-
-service { 'nginx':
-ensure     => running,
-enable     => true,
-hasrestart => true,
-require    => Package['nginx'],
-subscribe  => File_line['add protocol'],
+-> file { '/var/www/html/index.html' :
+  content => 'Holberton School!',
 }
-
-file {'/var/www/html/index.html':
-ensure  =>  present,
-path    => '/var/www/html/index.html',
-content => 'Holberton School for the win yeah!\n',
+-> file_line { 'add header' :
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "\tadd_header X-Served-By ${hostname};",
+  after  => 'server_name _;',
 }
-
-file_line {'add protocol':
-ensure => present,
-path   => '/etc/nginx/sites-available/default',
-after  => 'listen 80 default_server;',
-line   => "\tadd_header X-Served-By ${hostname};",
+-> service { 'nginx':
+  ensure => running,
 }
