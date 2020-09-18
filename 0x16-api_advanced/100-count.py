60a690d2030c07cab5e61797e9ea2ccffea76166
daorejuela1 <daorejuela1@outlook.com>
2020-09-18 01:42:56 +0000

    added lower key condition

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -21,8 +21,7 @@ def count_words(subreddit, word_list, after=None, my_dict={}):
             return
         for children in post_list:
             title1 = children.get('data').get('title')
-            for word in set(word_list):
-                word = word.lower()
+            for word in word_list:
                 try:
                     my_dict[word] += title1.lower().split().count(word.lower())
                 except KeyError:
