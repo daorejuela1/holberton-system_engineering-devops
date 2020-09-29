7257b1e19ab54d6d2e4327204399eb70cb780ab3
daorejuela1 <daorejuela1@outlook.com>
2020-09-29 17:17:21 +0000

    added path to the bash commands

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
 # This script looks onto the wordpress config file and replaces its bad value
 exec { 'replace_wrong':
-  command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php"
+  command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php",
+  path    =>  '/bin:/usr/bin:/sbin:/usr/sbin:...'
 }
