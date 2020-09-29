03ac346c5f03920971563ac581267223554de639
daorejuela1 <daorejuela1@outlook.com>
2020-09-29 17:13:33 +0000

    used exec to avoid issues with puppet not having stdlib installed

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,4 @@
-file_line { 'wp-settings':
-  path    => '/var/www/html/wp-settings.php',
-  replace => true,
-  line    => "require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );",
-  match   => "require_once( ABSPATH . WPINC . '/class-wp-locale.php' );"
+# This script looks onto the wordpress config file and replaces its bad value
+exec { 'replace_wrong':
+  command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php"
 }
