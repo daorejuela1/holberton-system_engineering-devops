b6b65052f984c04fea60c73c32e550ccd944da5c
daorejuela1 <daorejuela1@outlook.com>
2020-10-05 20:23:31 +0000

    fixed typo for sed command and removed nginx notification

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,6 @@
-# Ensures that the limit of open files for nginx is high
+#Removes every configuration for the domain holberton in limits
 exec { 'fix--reads-holbertonuser':
-  path     => '/usr/bin:/usr/sbin:/bin',
-  provider => shell,
-  notify   => Service['nginx'],
-  command  => 'sed -i \'/holberton/c\' /etc/security/limits.conf'
+  path     =>  '/usr/bin:/usr/sbin:/bin',
+  provider =>  shell,
+  command  =>  'sed -i \'/holberton/c\\\' /etc/security/limits.conf'
 }
-service { 'nginx' :
-  ensure => 'running',
-  enable => 'true'
-}
-
